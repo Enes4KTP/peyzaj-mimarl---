2f6c9bdeb4b3c7df6da83cbea4b7e768284c9dbf
caglaaksoy <caglaaksoy2001@gmail.com>
2024-05-04 15:20:41 +0300

    Maindeki dosyalar düzenlendi

--- peyzaj/src/Components/pages/Bitkiler.jsx
+++ peyzaj/src/Components/pages/Bitkiler.jsx
@@ -1,7 +1,37 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 function Bitkiler() {
-  return <div>Bitkiler</div>;
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <Box
+          sx={{
+            width: "200px",
+            height: "300px",
+            bgcolor: "grey",
+            marginRight: "20px",
+          }}
+        ></Box>
+        <Box
+          sx={{
+            width: "200px",
+            height: "300px",
+            bgcolor: "grey",
+            marginRight: "20px",
+          }}
+        ></Box>
+        <Box
+          sx={{
+            width: "200px",
+            height: "300px",
+            bgcolor: "grey",
+            marginRight: "20px",
+          }}
+        ></Box>
+      </Box>
+    </div>
+  );
 }
 
 export default Bitkiler;
